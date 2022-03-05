c02b8dc8f498499528be1546d63a7645d053a768
saimhasan09 <saimhasannahid@gmail.com>
2022-03-06 02:09:41 +0600

    update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -20,7 +20,7 @@ const Navbar = () => {
   return (
     <nav>
       <h3>
-        <Link to="/">Messenger</Link>
+        <Link to="/">Flash Chat 24</Link>
       </h3>
       <div>
         {user ? (
